7ee19f24b90858fab0fedb3eaba8b622a77a740e
PSahu0410 <152206393+PSahu0410@users.noreply.github.com>
2025-06-01 18:41:38 +0530

    Updated code.py

--- code.py
+++ code.py
@@ -1,46 +1,55 @@
 import socket
 import sys
 from datetime import datetime
-from time import strftime
-
-target =input("Enter the host to SCAN: ")
-host = socket.gethostbyname(target) #Gets host's IP
-
-file = open("Port_Scanner.txt","w")
-
-initial_time= datetime.now()
-print("Start Time: {}".format(initial_time.strftime("%H:%M:%S")))
-file.write("Start Time: {}\n".format(initial_time.strftime("%H:%M:%S")))
-try:
-    for port in range(1,1025):
-        sock = socket.socket()
-        sock.settimeout(0.1)
-        result = sock.connect_ex((host,port))
-        if result==0:
-            try:
-                print("Port no. {} Open Protocol {}".format(port,socket.getservbyport(port,"tcp")))
-                file.write("Port no. {} Open Protocol {}\n".format(port,socket.getservbyport(port,"tcp")))
-            except Exception as e:
-                print("Port no. {} Open Protocol {}".format(port, "Unknown"))
-                file.write("Port no. {} Open Protocol {}\n".format(port, "Unknown"))
-        # else:
-        #     print("Port no. {} closed".format(port))
-
-except socket.gaierror:
-    print("Hostname could not be resolved")
-    file.write("Hostname could not be resolved")
-
-except socket.error:
-    print("Could not connet to Server...Exiting")
-    file.write("Could not connet to Server...Exiting")
-
-    sys.exit()
+import argparse
 
 
-final_time = datetime.now()
-print("End Time: {}".format(final_time.strftime("%H:%M:%S")))
-file.write("End Time: {}\n".format(final_time.strftime("%H:%M:%S")))
+with open("Port_Scanner.txt", "w") as file:
+    parser = argparse.ArgumentParser(description="This is a basic Port Scanner")
 
-total_time = final_time-initial_time
-print("Total time is {}".format(total_time))
-file.write("Total time is {}\n".format(total_time))
+    parser.add_argument("-s", "--Start", help="Enter the starting port", type=int, default=1)
+    parser.add_argument("-e", "--End", help="Enter the ending port", type=int, default=1024)
+    parser.add_argument("-t", "--Target", help="Enter the target IP/URL", required=True)
+    args = parser.parse_args()
+
+    try:
+        host = socket.gethostbyname(args.Target)  # Get host's IP
+    except socket.gaierror:
+        print("Hostname could not be resolved.")
+        file.write("Hostname could not be resolved.\n")
+        sys.exit()
+
+    initial_time = datetime.now()
+    print(f"Start Time: {initial_time.strftime('%H:%M:%S')}")
+    file.write(f"Start Time: {initial_time.strftime('%H:%M:%S')}\n")
+
+    try:
+        for port in range(args.Start, args.End + 1):
+            #print(f"Scanning port {port}")
+            sock = socket.socket()
+            sock.settimeout(1)
+            result = sock.connect_ex((host, port))
+
+            if result == 0:
+                try:
+                    service = socket.getservbyport(port, "tcp")
+                except:
+                    service = "Unknown"
+
+                print(f"Port {port} is OPEN | Protocol: {service}")
+                file.write(f"Port {port} is OPEN | Protocol: {service}\n")
+
+            sock.close()
+
+    except socket.error:
+        print("Could not connect to server. Exiting.")
+        file.write("Could not connect to server. Exiting.\n")
+        sys.exit()
+
+    final_time = datetime.now()
+    print(f"End Time: {final_time.strftime('%H:%M:%S')}")
+    file.write(f"End Time: {final_time.strftime('%H:%M:%S')}\n")
+
+    total_time = final_time - initial_time
+    print(f"Total time taken: {total_time}")
+    file.write(f"Total time taken: {total_time}\n")
